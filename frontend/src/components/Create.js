92eac0519d85aa3241b6ab851bff896f1839d17c
azhadsyed <azhadsyed17@gmail.com>
2021-01-05 09:54:57 -0500

    added "Create" component

---
+++ frontend/src/components/Create.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+//into the wind - blue wednesday
+
+class Create extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentStep: 1,
+      prompt: [],
+      passwordSet: [],
+      passwordVerify: [],
+      title: "",
+      content: "",
+    };
+    this.carousel = ["foo", "bar", "baz"];
+  }
+
+  prevStep = () => {
+    let currentStep = this.state.currentStep;
+    currentStep = currentStep < 2 ? 1 : currentStep - 1;
+    this.setState({ currentStep: currentStep });
+  };
+
+  nextStep = () => {
+    let currentStep = this.state.currentStep;
+    currentStep = currentStep > 2 ? 3 : currentStep + 1;
+    this.setState({ currentStep: currentStep });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.carousel[this.state.currentStep - 1]}
+        <p></p>
+        <button onClick={this.prevStep}>Back</button>
+        <button onClick={this.nextStep}>Next</button>
+      </div>
+    );
+  }
+}
+
+// let recordingPrompt = new Step("recordingPrompt", "Perform your musical prompt.", "Use the keyboard to practice. \n Press record when you're ready. \n When you're done, press stop.")
+// let recordingPassword = new Step("recordingPassword", "Perform your musical response.", "Press record again to save a response to your prompt. Press stop when you're done.")
+// let confirmingPassword = new Step("confirmingPassword", "Confirm your musical response.")
+
+export default Create;

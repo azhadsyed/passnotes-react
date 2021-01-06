f0e2b4399e6f82e4cf9281e6671e1ff47452010f
azhadsyed <azhadsyed17@gmail.com>
2021-01-06 14:16:48 -0500

    homebrewed the piano, no longer need react-piano

--- frontend/src/components/Authenticate.js
+++ frontend/src/components/Authenticate.js
@@ -3,6 +3,8 @@ import Keyboard from "./Keyboard";
 import "./Authenticate.css";
 
 const Authenticate = (props) => {
+  const hue = `hsl(${Math.floor(Math.random() * 255)}, 100%, 80%)`;
+
   return (
     <section className="authenticate">
       <div>{props.title}</div>
@@ -10,7 +12,7 @@ const Authenticate = (props) => {
         Listen to the musical prompt left by the author, and perform the
         response on your QWERTY keyboard
       </div>
-      <Keyboard />
+      <Keyboard hue={hue} />
       <div>
         <button>Play Again</button>
         <button>Continue</button>
--- frontend/src/components/Keyboard.js
+++ frontend/src/components/Keyboard.js
@@ -1,72 +1,68 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { notesInOrder, keyToNote } from "./Constants";
 import "./Keyboard.css";
 
-const Keyboard = () => {
-  let iWhite = 0;
-  const keys = notesInOrder.map((note) => {
-    let keyType = note.length > 2 ? "black key" : "white key";
-    let left;
-    if (keyType === "white key") {
-      left = iWhite * 40 + "px";
-      iWhite++;
-    } else {
-      left = iWhite * 40 - 15 + "px";
-    }
-    return (
-      <div
-        key={note}
-        className={keyType}
-        id={note}
-        style={{ left: left }}
-      ></div>
-    );
-  });
+const Keyboard = (props) => {
+  let n = notesInOrder.length;
+  const [keyboardState, setKeyboardState] = useState(Array(n).fill(0));
 
-  const hue = `hsl(${Math.floor(Math.random() * 255)}, 100%, 80%)`;
-
+  //playKeyboard modifies the "state" of the Keyboard, where 1 means a key is pressed down
   const playKeyboard = (e) => {
     const note = keyToNote[e.key];
-    const type = e.type;
-    if (note) {
-      if (type === "keydown") {
-        console.log(note);
-        console.log(keys.note);
-      } else if (type === "keyup") {
-        console.log("bar");
-      }
+    const noteIndex = notesInOrder.indexOf(note);
+    let newKeyboardState = keyboardState.slice();
+    if (e.type === "keydown") {
+      newKeyboardState[noteIndex] = 1;
+    } else {
+      newKeyboardState[noteIndex] = 0;
     }
+    setKeyboardState(newKeyboardState);
   };
 
   useEffect(() => {
     document.addEventListener("keydown", playKeyboard);
     document.addEventListener("keyup", playKeyboard);
-
     return function cleanup() {
       document.removeEventListener("keydown", playKeyboard);
       document.removeEventListener("keyup", playKeyboard);
     };
   });
 
+  let iWhite = 0;
+  const keys = notesInOrder.map((note, index) => {
+    const keyState = keyboardState[index];
+    const className = note.length > 2 ? "black key" : "white key";
+    const defaultColor = note.length > 2 ? "black" : "white";
+    const statefulColor = keyState ? props.hue : defaultColor;
+    let left;
+    if (defaultColor === "white") {
+      left = 40 * iWhite + "px";
+      iWhite++;
+    } else {
+      left = 40 * iWhite - 15 + "px";
+    }
+    return (
+      <Key
+        key={note}
+        color={statefulColor}
+        id={note}
+        className={className}
+        keyState={keyState}
+        left={left}
+      />
+    );
+  });
+
   return <div className="keyboard">{keys}</div>;
 };
 
-export default Keyboard;
+// the key's color is dependent on its class and its keyState
+const Key = (props) => {
+  const style = {
+    backgroundColor: props.color,
+    left: props.left,
+  };
+  return <div className={props.className} id={props.id} style={style}></div>;
+};
 
-/*
-[ ] Visible:
-  [x] map through notesInOrder and create keys
-  [x] style the keys
-  [x] layout the keys absolutely in a relative div 
-  (note - all the onKeyDown/window event listener examples I found were stateful,
-     
-  [ ] lightUp the keys on KeyDown
-[ ] Playable:
-  [ ] ONE SYNTH that triggers attacks and releases
-[ ] Recordable
-  [ ] push to buffer on keyDown
-[ ] Verifiable
-  [ ] fetch api requests
-[ ] Pretty
-  [ ] Design w/Amara
-*/
+export default Keyboard;

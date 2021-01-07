5be1806afc7f63d992132bc0689a368b60504421
azhadsyed <azhadsyed17@gmail.com>
2021-01-07 17:17:00 -0500

    lifting hue/synth initialization out of Authenticate

--- frontend/src/components/Authenticate.js
+++ frontend/src/components/Authenticate.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Keyboard from "./Keyboard";
-import { Synth } from "tone";
+import { keyToNote } from "./Constants";
 import "./Authenticate.css";
 
+//
+
 const Authenticate = (props) => {
-  const hue = `hsl(${Math.floor(Math.random() * 255)}, 100%, 80%)`;
-  const synth = new Synth().toDestination();
+  const [buffer, setBuffer] = useState([]);
+
+  const hue = props.hue;
+  const synth = props.synth;
+
+  const writeToBuffer = (e) => {
+    const newBuffer = buffer.slice();
+    const note = keyToNote[e.key]; // code review: this is repeated code both here and in Keyboard
+    newBuffer.push([Date.now(), note]);
+    setBuffer(newBuffer);
+  };
+
+  const clearBuffer = () => {
+    console.log(buffer);
+    setBuffer([]);
+  };
+
+  useEffect(() => {
+    //on component mount...
+    document.addEventListener("keydown", writeToBuffer);
+
+    //on unmount...
+    return function cleanup() {
+      document.removeEventListener("keydown", writeToBuffer);
+    };
+  });
 
   return (
     <section className="authenticate">
@@ -16,7 +42,7 @@ const Authenticate = (props) => {
       </div>
       <Keyboard hue={hue} synth={synth} />
       <div>
-        <button>Play Again</button>
+        <button onClick={clearBuffer}>Play Again</button>
         <button>Continue</button>
       </div>
     </section>

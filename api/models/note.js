4645009c7ee4ae23012950678dd68ed6e33744da
Amara Jaeger <amara@Amaras-Air.MG8702>
2021-01-12 12:11:53 -0500

    replies are threading- rendered in a list with newlines between them.  added css stylesheet for viewReply.

--- api/models/note.js
+++ api/models/note.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const noteSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   title: String,
-  // content: String,
   content: Array,
   prompt: Array,
   password: Array,
--- frontend/src/components/View.js
+++ frontend/src/components/View.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Reply from "./Reply.js";
+import "./ViewReply.css";
 
 const View = (props) => {
   let state;
@@ -10,20 +11,27 @@ const View = (props) => {
     state = {
       id: "5fdac78eca70f62f4c35994e",
       title: "test title",
-      content: ["green, blue, red"],
+      content: ["green", "blue", "red"],
     };
   }
   const { id, title, content } = state;
   // set up a variable to indicate if reply has been clicked
   const [replyClicked, setReplyState] = useState(0);
+  const renderContent = content.join("\n");
+
+  const NewlineText = (props) => {
+    const text = props.text;
+    return <div>{text}</div>;
+  };
 
   return (
     <section className="View">
       <div className="instructions">
         View or reply to secret message: {title}
       </div>
-      <div>{content}</div>
-      {/* <div>replyClicked has state = {replyClicked}</div> */}
+      <div className="viewNoteContent">
+        <NewlineText text={renderContent} />
+      </div>
       <button onClick={() => setReplyState(1)}> Reply</button>
       {replyClicked === 1 ? <Reply id={id} content={content} /> : <div></div>}
     </section>

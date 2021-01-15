558e7ce3a02e5f62f28e5ef521684ce382b73edb
Amara Jaeger <amara.jaeger@gmail.com>
2021-01-15 11:18:02 -0500

    fixed broken filepaths

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Authenticate from "./components/Authenticate";
 import Create from "./components/Create";
 import Noteboard from "./components/Noteboard.js";
-import View from "./components/View";
 import ViewReply from "./components/ViewReply.js";
 import { sendHttpRequest } from "./components/utilities/helpers.js";
 import "./index.css";
@@ -45,9 +44,6 @@ class App extends React.Component {
           <Link to="/Authenticate">Authenticate</Link>
         </li>
         <li>
-          <Link to="/View">View</Link>
-        </li>
-        <li>
           <Link to="/ViewReply">View & Reply (dev)</Link>
         </li>
         <li>
@@ -57,7 +53,6 @@ class App extends React.Component {
         <Switch>
           <Route component={Authenticate} path="/Authenticate" />
           <Route component={Create} path="/Create" />
-          <Route component={View} path="/View" />
           <Route component={ViewReply} path="/ViewReply" />
           <Route path="/">
             <Noteboard
